f3275e7309784ec7dcd7158489d83b4e8e48c5df
Yu Huaping <huaping.yu@mintegral.com>
2025-03-18 14:56:13 +0800

    feat: x

--- internal/module/workspace/service/setting.go
+++ internal/module/workspace/service/setting.go
@@ -15,8 +15,7 @@ func (s *Service) GetSetting(ctx context.Context, key model.SettingKey, value an
 		return
 	}
 	if err = json.Unmarshal(setting.Value.RawMessage, value); err != nil {
-		data, _ := setting.Value.MarshalJSON()
-		logrus.WithContext(ctx).Errorf("解析json失败, data: %s", err, string(data))
+		logrus.WithContext(ctx).Errorf("解析json失败, err:%v", err)
 		return
 	}
 	return
