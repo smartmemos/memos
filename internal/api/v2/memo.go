a23accb28351bf34b8e4809e77b9c347d0d92aff
yearnfar <yearnfar@gmail.com>
2025-08-03 02:27:52 +0800

    feat: memos

--- internal/api/v2/memo.go
+++ internal/api/v2/memo.go
@@ -2,11 +2,14 @@ package v2
 
 import (
 	"context"
+	"fmt"
 
 	"connectrpc.com/connect"
 	"github.com/samber/do/v2"
+	"google.golang.org/protobuf/types/known/timestamppb"
 
 	"github.com/smartmemos/memos/internal/memos"
+	"github.com/smartmemos/memos/internal/memos/model"
 	v2pb "github.com/smartmemos/memos/internal/proto/api/v2"
 	modelpb "github.com/smartmemos/memos/internal/proto/model"
 )
@@ -24,5 +27,30 @@ func NewMemoService(i do.Injector) (*MemoService, error) {
 
 func (s *MemoService) CreateMemo(ctx context.Context, req *connect.Request[v2pb.CreateMemoRequest]) (resp *connect.Response[modelpb.Memo], err error) {
 
+	resp = connect.NewResponse(convertMemoToProto(&model.Memo{}))
 	return
+}
+
+func (s *MemoService) ListMemos(ctx context.Context, req *connect.Request[v2pb.ListMemosRequest]) (resp *connect.Response[v2pb.ListMemosResponse], err error) {
+	resp = connect.NewResponse(&v2pb.ListMemosResponse{
+		Memos: []*modelpb.Memo{
+			convertMemoToProto(&model.Memo{}),
+		},
+	})
+	return
+}
+
+func (s *MemoService) GetMemo(ctx context.Context, req *connect.Request[v2pb.GetMemoRequest]) (resp *connect.Response[modelpb.Memo], err error) {
+
+	resp = connect.NewResponse(convertMemoToProto(&model.Memo{}))
+	return
+}
+
+func convertMemoToProto(memo *model.Memo) *modelpb.Memo {
+	return &modelpb.Memo{
+		Name:       fmt.Sprintf("memos/%d", memo.ID),
+		Content:    memo.Content,
+		CreateTime: timestamppb.New(memo.CreatedAt),
+		UpdateTime: timestamppb.New(memo.UpdatedAt),
+	}
 }
---
+++ internal/memos/model/memo.go
@@ -0,0 +1,56 @@
+package model
+
+import "github.com/smartmemos/memos/internal/pkg/db"
+
+type Memo struct {
+	db.Model
+
+	UID          string
+	ParentID     int64
+	RelationType RelationType
+	CreatorID    int64
+	Content      string
+	Payload      MemoPayload `gorm:"serializer:json"`
+	Tags         []string    `gorm:"serializer:json"`
+	Pinned       bool
+	Visibility   Visibility
+	Status       string
+}
+
+func (Memo) TableName() string {
+	return TableMemo
+}
+
+type MemoPayload struct {
+	Property *MemoPayloadProperty `json:"property"`
+	Location *MemoPayloadLocation `json:"location"`
+	Tags     []string             `json:"tags"`
+}
+
+type MemoPayloadLocation struct {
+	Placeholder string  `json:"placeholder"`
+	Latitude    float64 `json:"latitude"`
+	Longitude   float64 `json:"longitude"`
+}
+
+type MemoPayloadProperty struct {
+	HasLink            bool     `json:"has_link"`
+	HasTaskList        bool     `json:"has_task_list"`
+	HasCode            bool     `json:"has_code"`
+	HasIncompleteTasks bool     `json:"has_incomplete_tasks"`
+	References         []string `json:"references"`
+}
+
+type FindMemoFilter struct {
+	db.BaseFilter
+
+	ID              int64
+	Pid             int64
+	ParentIDs       []int64
+	CreatorID       int64
+	ExcludeComments bool
+	ExcludeContent  bool
+	Status          string
+
+	VisibilityList []Visibility
+}
