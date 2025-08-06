16d359393ce3d36b1ece619ea9b1a96ae42663b7
Yu Huaping <huaping.yu@mintegral.com>
2025-08-06 20:12:48 +0800

    feat: 结构体字段名和数据库字段不一致

--- internal/pkg/db/model.go
+++ internal/pkg/db/model.go
@@ -6,7 +6,7 @@ import (
 
 // Model 基础结构
 type Model struct {
-	ID        int64     `gorm:"primary_key" json:"id,string"`     // 主键ID
-	CreatedAt time.Time `gorm:"autoCreateTime" json:"created_at"` // 创建时间
-	UpdatedAt time.Time `gorm:"autoUpdateTime" json:"updated_at"` // 更新时间
+	ID        int64     `gorm:"primary_key" json:"id,string"`                       // 主键ID
+	CreatedAt time.Time `gorm:"column:created_ts;autoCreateTime" json:"created_at"` // 创建时间
+	UpdatedAt time.Time `gorm:"column:updated_ts;autoUpdateTime" json:"updated_at"` // 更新时间
 }
